03103e0dbedaea6f20a20d7b48ef272294b2780e
ghYoon93 <gh.yoon93@gmail.com>
2020-04-20 17:07:56 +0900

    2.4 Hello Controller 코드를 롬복으로 전환하기

---
+++ src/test/java/com/ghyoon/webservice/practice2/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,23 @@
+package com.ghyoon.webservice.practice2.web.dto;
+
+
+import com.ghyoon.webservice.practice2.web.HelloResponseDto;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void 롬복_기능_테스트(){
+        //given
+        String name= "사과";
+        int amount= 3000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo("사과");
+        assertThat(dto.getAmount()).isEqualTo(3000);
+    }
+}
